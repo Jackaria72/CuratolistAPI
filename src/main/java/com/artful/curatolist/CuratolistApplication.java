765335f5c5f362452c5075e516cae1c807aa9e10
Jackaria72 <jack.goodwill@hotmail.co.uk>
2025-03-11 12:30:54 +0000

    Initial commit

---
+++ src/main/java/com/artful/curatolist/CuratolistApplication.java
@@ -0,0 +1,13 @@
+package com.artful.curatolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuratolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuratolistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artful/curatolist/CuratolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artful.curatolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuratolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
