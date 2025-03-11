1293357994c35aa2997f550d27becd45583e392d
Jack <jack.goodwill@hotmail.co.uk>
2025-03-11 20:52:02 +0000

    Feature/chicago client (#1)
    * Feat: create model class for Chicago page
    
    * feat: create Chicago client
    
    * Test: create test for getChicagoArtwork method

---
+++ src/main/java/com/artful/curatolist/client/ChicagoClient.java
@@ -0,0 +1,29 @@
+package com.artful.curatolist.client;
+
+import com.artful.curatolist.model.ChicagoPage;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Component
+public class ChicagoClient {
+
+    private final WebClient webClient;
+    private final String BASE_URL = "https://api.artic.edu/api/v1";
+
+    public ChicagoClient(WebClient.Builder webclientBuilder) {
+        this.webClient = webclientBuilder.baseUrl(BASE_URL).build();
+    }
+
+    public Mono<ChicagoPage> getChicagoArtwork(int page, int limit) {
+
+        return webClient.get().uri(uriBuilder -> uriBuilder
+                .path("/artworks/search")
+                .queryParam("query[term][is_public_domain]","true")
+                .queryParam("fields", "id,title,artist_title,date_start,date_end,date_display,image_id,is_public_domain")
+                .queryParam("page", page)
+                .queryParam("limit", limit)
+                .build())
+                .retrieve().bodyToMono(ChicagoPage.class);
+    }
+}
---
+++ src/main/java/com/artful/curatolist/model/ChicagoPage.java
@@ -0,0 +1,19 @@
+package com.artful.curatolist.model;
+
+import java.util.List;
+
+public record ChicagoPage(PageInfo pagination, List<ChicagoArt> data) {
+    public record PageInfo(
+            int total,
+            int total_pages
+    ){}
+    public record ChicagoArt(
+            int id,
+            String title,
+            String artist_title,
+            int date_start,
+            int date_end,
+            String date_display,
+            String image_id
+    ){}
+}
