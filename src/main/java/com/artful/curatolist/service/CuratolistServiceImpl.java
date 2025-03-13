6435fc74a39a115c1ec2782ce97e9bcbed6e44db
Jackaria72 <jack.goodwill@hotmail.co.uk>
2025-03-13 15:33:51 +0000

    Feat: create Service impl

---
+++ src/main/java/com/artful/curatolist/service/CuratolistServiceImpl.java
@@ -0,0 +1,51 @@
+package com.artful.curatolist.service;
+
+import com.artful.curatolist.client.ChicagoClient;
+import com.artful.curatolist.client.HarvardClient;
+import com.artful.curatolist.mapper.ChicagoMapper;
+import com.artful.curatolist.mapper.HarvardMapper;
+import com.artful.curatolist.model.CLArtwork;
+import com.artful.curatolist.model.CLPage;
+import com.artful.curatolist.model.ChicagoPage;
+import com.artful.curatolist.model.HarvardPage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Stream;
+
+@Service
+public class CuratolistServiceImpl implements CuratolistService{
+
+    @Autowired
+    ChicagoClient chicagoClient;
+    @Autowired
+    ChicagoMapper chicagoMapper;
+    @Autowired
+    HarvardClient harvardClient;
+    @Autowired
+    HarvardMapper harvardMapper;
+
+    @Override
+    public CLPage getArt(int page, int limit) {
+        int halfLimit = limit/2;
+
+        HarvardPage harvardRaw = harvardClient.getHarvardArtwork(page, halfLimit).block();
+        ChicagoPage chicagoRaw = chicagoClient.getChicagoArtwork(page, halfLimit).block();
+        List<CLArtwork> harvard = harvardMapper.mapHarvardArt(harvardRaw);
+        List<CLArtwork> chicago = chicagoMapper.mapChicagoArt(chicagoRaw);
+        CLPage.PageInfo pageInfo = new CLPage.PageInfo(
+                chicagoRaw.pagination().total(),
+                chicagoRaw.pagination().total_pages(),
+                harvardRaw.info().totalrecords(),
+                harvardRaw.info().pages());
+
+        List<CLArtwork> combined = Stream.of(harvard, chicago)
+                .flatMap(Collection::stream)
+                .toList();
+
+        return new CLPage(pageInfo, combined);
+    }
+}
