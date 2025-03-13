b4065921e7972303bacbe3cf2bb112c39d27baa7
Jackaria72 <jack.goodwill@hotmail.co.uk>
2025-03-13 11:39:48 +0000

    Feat: create ChicagoMapper

---
+++ src/main/java/com/artful/curatolist/mapper/ChicagoMapper.java
@@ -0,0 +1,25 @@
+package com.artful.curatolist.mapper;
+
+import com.artful.curatolist.model.CLArtwork;
+import com.artful.curatolist.model.ChicagoPage;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class ChicagoMapper {
+
+    public List<CLArtwork> mapChicagoArt(ChicagoPage page) {
+        return page.data().stream()
+                .map(art -> new CLArtwork(
+                        "AIC"+String.valueOf(art.id()),
+                        art.title(),
+                        art.artist_title(),
+                        String.valueOf(art.date_start())+" - "+String.valueOf(art.date_end()),
+                        art.date_display(),
+                        art.image_id(),
+                        "Art Institue of Chicago"
+                )).collect(Collectors.toList());
+    }
+}
