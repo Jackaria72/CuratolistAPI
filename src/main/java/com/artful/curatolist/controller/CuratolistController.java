b59a4fbe21a61c5f95b8cf212ec96774ce63bc3b
Jack <jack.goodwill@hotmail.co.uk>
2025-03-14 18:15:43 +0000

    Fix/blocking (#16)
    * Fix: fix blocking in service layer
    
    * Fix: fix blocking in controller layer
    
    * Fix: fix tests

--- src/main/java/com/artful/curatolist/controller/CuratolistController.java
+++ src/main/java/com/artful/curatolist/controller/CuratolistController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/curatolist/api/v1")
@@ -18,8 +19,8 @@ public class CuratolistController {
     private CuratolistService curatolistService;
 
     @GetMapping
-    public ResponseEntity<CLPage> getArtwork(@RequestParam(name = "page", defaultValue = "1") int page,
-                                             @RequestParam(name = "limit", defaultValue = "10") int limit) {
+    public ResponseEntity<Mono<CLPage>> getArtwork(@RequestParam(name = "page", defaultValue = "1") int page,
+                                                   @RequestParam(name = "limit", defaultValue = "10") int limit) {
        return new ResponseEntity<>(curatolistService.getArt(page, limit), HttpStatus.OK);
     }
 }
