9f5c3e952477e7c6a96ae5bb8f6e0c2938066644
Jackaria72 <jack.goodwill@hotmail.co.uk>
2025-03-14 19:23:11 +0000

    Feat: create GlobalExceptionHandler

---
+++ src/main/java/com/artful/curatolist/controller/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.artful.curatolist.controller.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<String> handleResourcesNotFound(ResourcesNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+}
