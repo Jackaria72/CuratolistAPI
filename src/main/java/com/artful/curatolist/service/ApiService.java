ae93487acebe93747b27168383ad9691eabce1a5
Jackaria72 <jack.goodwill@hotmail.co.uk>
2025-03-17 01:34:57 +0000

    Feat: create Api Service Interface

---
+++ src/main/java/com/artful/curatolist/service/ApiService.java
@@ -0,0 +1,8 @@
+package com.artful.curatolist.service;
+
+import com.artful.curatolist.model.CLPage;
+import reactor.core.publisher.Mono;
+
+public interface ApiService {
+    Mono<CLPage> getArt(int page);
+}
